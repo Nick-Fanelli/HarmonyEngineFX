af9bd7b49332cf2931e84da5c04ecb455a47be64
Nick <nickfanelli5@gmail.com>
2020-06-24 18:25:49 -0400

    Added in tree view icons

--- src/main/java/com/harmony/engine/io/editor/CodeEditor.java
+++ src/main/java/com/harmony/engine/io/editor/CodeEditor.java
@@ -1,15 +1,26 @@
 package com.harmony.engine.io.editor;
 
+import com.harmony.engine.EngineController;
 import com.harmony.engine.Harmony;
 import javafx.application.Platform;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 import java.io.File;
 
 public class CodeEditor implements Runnable {
+
+    public static final ImageView FOLDER_ICON = new ImageView(
+            new Image(CodeEditor.class.getResource("/images/icons/folder-icon.png").toExternalForm(), 18, 18, true, true)
+    );
+
+    public static final ImageView PYTHON_ICON = new ImageView(
+            new Image(CodeEditor.class.getResource("/images/icons/python-icon.png").toExternalForm(), 18, 18, true, true)
+    );
 
     private static Thread codeEditorThread = null;
     private static Runnable syncRunnable;
@@ -53,6 +64,9 @@ public class CodeEditor implements Runnable {
 
         loadAllChildren(root, scriptsDirectory);
 
+
+        root.setGraphic(CodeEditor.FOLDER_ICON);
+
         root.setExpanded(true);
         codeFileList.setRoot(root);
         codeFileList.setEditable(true);
@@ -68,7 +82,15 @@ public class CodeEditor implements Runnable {
             if(child.getName().startsWith(".")) continue;
             TreeItem<String> childItem = new TreeItem<>(child.getName());
             parent.getChildren().add(childItem);
-            if(child.isDirectory()) loadAllChildren(childItem, child);
+            if(child.isDirectory()) {
+                childItem.setGraphic(CodeEditor.FOLDER_ICON);
+                loadAllChildren(childItem, child);
+            } else {
+                try {
+                    if (child.getName().split("\\.")[1].equals("py"))
+                        childItem.setGraphic(CodeEditor.PYTHON_ICON);
+                } catch (Exception ignored) {}
+            }
         }
     }
 

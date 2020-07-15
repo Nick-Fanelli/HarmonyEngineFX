a2dd0987253e979e80ff12dfb57f1a3f973f66ce
Nick <nickfanelli5@gmail.com>
2020-07-15 14:25:03 -0400

    Set-up instance for everyting

--- src/main/java/com/harmony/engine/bridge/Bridge.java
+++ src/main/java/com/harmony/engine/bridge/Bridge.java
@@ -5,10 +5,27 @@
 
 package com.harmony.engine.bridge;
 
+import com.harmony.engine.data.ProjectData;
+import com.harmony.engine.io.editor.state.State;
+
+import java.util.ArrayList;
+
 public class Bridge {
 
-    public Bridge() {
+    public static String name;
+    public static String version;
 
+    public static ArrayList<State> states;
+
+    public static void update() {
+        Bridge.name = ProjectData.projectName;
+        Bridge.version = ProjectData.versionID;
+
+        Bridge.states = new ArrayList<>(ProjectData.states);
+    }
+
+    public static void run() {
+        
     }
 
 }

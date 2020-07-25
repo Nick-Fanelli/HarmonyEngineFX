6b50a9b8606c76d40df18051cc1533f5d40d51b2
Nick <nickfanelli5@gmail.com>
2020-07-25 12:06:17 -0400

    Fixed the NetResource download

--- src/main/java/com/harmony/engine/data/networking/resource/DemoResource.java
+++ src/main/java/com/harmony/engine/data/networking/resource/DemoResource.java
@@ -5,13 +5,15 @@
 
 package com.harmony.engine.data.networking.resource;
 
+import java.util.HashMap;
+
 public class DemoResource extends NetResource {
 
     public static final DemoResource RESOURCE_CONTEXT = new DemoResource();
 
     public DemoResource() {
-        super("https://harmonyengines.com/resources/demo", new String[] {
-                "Tileset.png"
-        });
+        super("https://harmonyengines.com/resources/demo", new HashMap<>() {{
+            put("Tileset.png", "Textures");
+        }});
     }
 }
--- src/main/java/com/harmony/engine/data/networking/resource/NetResource.java
+++ src/main/java/com/harmony/engine/data/networking/resource/NetResource.java
@@ -8,19 +8,23 @@ package com.harmony.engine.data.networking.resource;
 import com.harmony.engine.data.networking.NetUtils;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class NetResource {
 
     public final String URL_LOCATION;
-    public final String[] FILE_LOCATIONS;
+    public final HashMap<String, String> FILE_LOCATIONS;
 
-    public NetResource(String urlLocation, String[] fileLocations) {
+    public NetResource(String urlLocation, HashMap<String, String> fileLocations) {
         this.URL_LOCATION = urlLocation;
         this.FILE_LOCATIONS = fileLocations;
     }
 
     public void downloadTo(File directory) {
-        NetUtils.downloadAllFiles(URL_LOCATION, FILE_LOCATIONS, directory);
+        for(Map.Entry<String, String> entry : FILE_LOCATIONS.entrySet()) {
+            NetUtils.downloadFile(URL_LOCATION + "/" + entry.getKey(), directory + File.separator + entry.getValue() + File.separator + entry.getKey());
+        }
     }
 
 }

4e06900ebf6631c703ec1c91287b08dde331bd85
Nick <nickfanelli5@gmail.com>
2020-08-08 13:04:40 -0400

    Implemented the new event system.

--- src/main/java/com/harmony/engine/io/SelectionModel.java
+++ src/main/java/com/harmony/engine/io/SelectionModel.java
@@ -5,12 +5,16 @@
 
 package com.harmony.engine.io;
 
+import com.harmony.engine.event.AbstractEventHandler;
+import com.harmony.engine.event.SelectionEventHandler;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SelectionModel<T> {
 
     public ArrayList<T> model = new ArrayList<>();
+    public AbstractEventHandler<SelectionEventHandler> handler = new AbstractEventHandler<>();
 
     @SafeVarargs
     public final void setSelection(T... selections) {
@@ -19,16 +23,26 @@ public class SelectionModel<T> {
     }
 
     @SafeVarargs
-    public final void addToSelection(T... selections) { model.addAll(Arrays.asList(selections)); }
+    public final void addToSelection(T... selections) {
+        model.addAll(Arrays.asList(selections));
+
+        for(SelectionEventHandler handler : handler.getEventHandlers()) handler.onSelectionChange();
+    }
 
     @SafeVarargs
     public final void removeFromSelection(T... selections) {
         for (T selection : selections) { model.remove(selection); }
+
+        for(SelectionEventHandler handler : handler.getEventHandlers()) handler.onSelectionChange();
     }
 
     public boolean contains(T item) { return model.contains(item); }
 
-    public void clear() { model.clear(); }
+    public void clear() {
+        model.clear();
+
+        for(SelectionEventHandler handler : handler.getEventHandlers()) handler.onSelectionChange();
+    }
 
     @Override
     public String toString() {
